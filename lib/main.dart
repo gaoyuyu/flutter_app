2a5bf607aba0d016d6c8f6930ea3302c75777917
gaoyuyu <gaoyuyu@9snail.com>
2019-01-28 18:45:53 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(title: "导航的数据传递和接受", home: FirstPage()));
+}
 
-class MyApp extends StatelessWidget {
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var card = new Card(
-      child: new Column(
-        children: <Widget>[
-          new ListTile(
-            title: new Text(
-              "address1",
-              style: new TextStyle(fontWeight: FontWeight.w500),
-            ),
-            subtitle: new Text(
-              "address1",
-            ),
-            leading: new Icon(Icons.live_help,color: Colors.blueAccent,),
-          ),
-          new Divider(),
-          new ListTile(
-            title: new Text(
-              "address2",
-              style: new TextStyle(fontWeight: FontWeight.w500),
-            ),
-            subtitle: new Text(
-              "address2",
-            ),
-            leading: new Icon(Icons.lightbulb_outline,color: Colors.blueAccent,),
-          ),
-          new Divider(),
-          new ListTile(
-            title: new Text(
-              "address3",
-              style: new TextStyle(fontWeight: FontWeight.w500),
-            ),
-            subtitle: new Text(
-              "address3",
-            ),
-            leading: new Icon(Icons.map,color: Colors.blueAccent,),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("找小姐姐要电话"),
+      ),
+      body: Center(
+        child: RouteButton(),
       ),
     );
-    return MaterialApp(
-        title: "Positiond",
-        home: new Scaffold(
-            appBar: new AppBar(
-              title: new Text("Positiond Widget"),
+  }
+}
+
+class RouteButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text("去找小姐姐"),
+      onPressed: () {
+        _navigateToXiaojiejie(context);
+      },
+    );
+  }
+}
+
+/**
+ * 加下划线表示是内部的方法
+ */
+_navigateToXiaojiejie(BuildContext context) async {
+  final result = await Navigator.push(
+      context, MaterialPageRoute(builder: (context) => Xiaojiejie()));
+  Scaffold.of(context).showSnackBar(SnackBar(content: Text(result)));
+}
+
+class Xiaojiejie extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("我是小姐姐"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("大长腿小姐姐"),
+              onPressed: () {
+                Navigator.pop(context, "大长腿小姐姐：123456");
+              },
             ),
-            body: new Center(
-              child: card,
-            )));
+            RaisedButton(
+              child: Text("小蛮腰小姐姐"),
+              onPressed: () {
+                Navigator.pop(context, "小蛮腰小姐姐：784581555");
+              },
+            ),
+            RaisedButton(
+              child: Text("圆脸小姐姐"),
+              onPressed: () {
+                Navigator.pop(context, "圆脸小姐姐：7451554544455");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
